f039de06a9ac55977df5d3e5803e7437132431cf
Enrico Mischorr <enrico@mischorr.de>
2019-02-15 21:18:38 +0100

    fix seeds

--- apps/data/priv/repo/seeds.exs
+++ apps/data/priv/repo/seeds.exs
@@ -1,6 +1,6 @@
 # Script for populating the database. You can run it as:
 #
-#     mix run priv/repo/seed.exs
+#     mix run priv/repo/seeds.exs
 #
 alias Data.Repo
 alias Data.Models.{Tenant, User}
@@ -8,15 +8,15 @@ alias Data.Models.{Tenant, User}
 # Basic Data
 IO.puts "== Seeding Database =="
 
-admin_tenant = Repo.insert(%Tenant{
+{:ok, admin_tenant} = Repo.insert(%Tenant{
   code: "admin",
   name: "Admin",
 }, on_conflict: :nothing)
 
-admin_user_change = User.changeset %User{}, %{
+admin_user_change = User.changeset(%User{}, %{
   name: "Admin",
   login: "admin",
   password: "admin",
   tenant_id: admin_tenant.id
-}
-admin_user = Repo.insert(admin_user_change, , on_conflict: :nothing)
+})
+admin_user = Repo.insert(admin_user_change, on_conflict: :nothing)
